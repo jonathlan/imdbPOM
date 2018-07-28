687eb6a89932db17afef42ecee8e593e1880e33c
Jonathan Solis <38994980+sacrofer@users.noreply.github.com>
2018-07-28 15:28:50 -0500

    Implement clickMovieByYear and getMovieByYear methods

--- src/com/imdb/pages/ImdbResultsPage.java
+++ src/com/imdb/pages/ImdbResultsPage.java
@@ -1,5 +1,8 @@
 package com.imdb.pages;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -10,19 +13,31 @@ public class ImdbResultsPage {
 		this.driver = driver2;
 	}
 	//Elements
-		WebElement resultsLbl; // //h1[@class='findHeader']
-		WebElement titlesLbl; // //h3[@class='findSectionHeader' and text() = 'Titles']
+//		WebElement resultsLbl; // //h1[@class='findHeader']
+//		WebElement titlesLbl; // //h3[@class='findSectionHeader' and text() = 'Titles']
 		
 		//Methods
 		public boolean at() {
 			return false;
 		}
 		
-		public void getMovieByYear(String movieName, String movieYear) {
-			
+		public void clickMovieByYear(String movieName, String movieYear) throws Exception {
+			WebElement correctElement = getMovieByYear(movieName, movieYear);
+			if (correctElement != null){				
+				correctElement.findElement(By.linkText(movieName)).click();
+				System.out.println(movieName + " from " + movieYear + " found");
+			} else {				
+				throw new Exception("Test FAILED - " + movieName + " from " + movieYear + " NOT found");				
+			}
 		}
 		
-		public void clickMovieByYear(String movieName, String movieYear) {
-			
+		public WebElement getMovieByYear (String movieName, String movieYear) {
+			By by = By.className("result_text");
+			List<WebElement> elements = driver.findElements(by);
+			for (WebElement element : elements) {
+				if (element.getText().contains(movieName) && element.getText().contains(movieYear))
+					return element;
+			}
+			return null;		
 		}
 }
